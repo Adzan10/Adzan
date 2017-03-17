bd5f1f3235f7c97a59e4d9cc1aa0ee980dae3153
Adzan10 <flukeadzan10@gmail.com>
2017-03-17 15:06:03 +0700

    Update coba.php

--- coba.php
+++ coba.php
@@ -1,11 +1,7 @@
-<?php
-$employeeData = '{   
-	"nim": 15.01.53.0004,    
-	"nama": "Mohammad_Adzan",    
-	"phone": "089683485709",    
-	"matkul": "web_service",
-	"consultant": false
-}';
-$obj = json_decode($employeeData);
-echo $obj->{'nama'};
-?>
+{   
+	"nim": "14.01.53.0004",    
+	"nama": "Mohammad Adzan",    
+	"email": "flukeadzan10@gmail.com",    
+	"no_hp": "089683485709",
+	"mata_kuliah":["m1","m2","m3"]
+}
